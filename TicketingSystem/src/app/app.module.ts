5322112949ac3c7f2d6480e83ff8a83a565dd7a2
fawziahAlaqil <85229759+fawziahAlaqil@users.noreply.github.com>
2021-08-04 14:01:45 +0300

    Update app.module.ts

--- TicketingSystem/src/app/app.module.ts
+++ TicketingSystem/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
+import { PagesModule } from './pages/pages.module';
 import { SharedModule } from './shared/shared.module';
 
 
@@ -17,8 +18,10 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     SharedModule,
     AuthModule,
-   
+    PagesModule
+
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
